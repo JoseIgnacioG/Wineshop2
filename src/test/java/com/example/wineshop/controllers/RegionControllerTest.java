9035b42944194d49fe86d9612756870c8f94ff27
Joel Hurtado <joel29102000@gmail.com>
2022-05-26 10:35:19 +0200

    prueba con gitignore y creacion de las clases de test

---
+++ src/test/java/com/example/wineshop/controllers/RegionControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.wineshop.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebClient
+class RegionControllerTest {
+
+    @Test
+    void all() {
+    }
+
+    @Test
+    void one() {
+    }
+}
